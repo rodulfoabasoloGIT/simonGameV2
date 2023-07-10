5a683012c6d68a7410c6269e10c97e62cf31c4a0
Abasol <ramzeeesss666@gmail.com>
2023-07-10 10:22:20 +0800

    jquery added

--- index.js
+++ index.js
@@ -5,70 +5,69 @@ let level = 0;
 let highScore = 0;
 const divColors = ["red", "green", "blue", "yellow"];
 
-$(document).on("keypress", function () {
+$(document).on("keypress", () => {
   if (level <= 0) {
-    $("h1").text("Game begins!");
+    $("h1").text("Game Start!");
     gameSequence();
   }
 });
 
-function gameSequence() {
-  //to keep the game going
+const gameSequence = () => {
   level++;
   $("h3").text(`level ${level}`);
   const randomNumber = Math.floor(Math.random() * 4);
   const randomColor = divColors[randomNumber];
-  colorAudio(randomColor);
-  $("#" + randomColor).addClass("pressed");
+  gameAudio(randomColor);
+  $("#" + randomColor).addClass("hover");
   setTimeout(() => {
-    $("#" + randomColor).removeClass("pressed");
-  }, 100);
+    $("#" + randomColor).removeClass("hover");
+  }, 150);
   correctPattern.push(randomColor);
-}
+};
 
-function colorAudio(randomColor) {
-  const audioPath = `sounds/${randomColor}.mp3`;
-  const audioPlay = new Audio(audioPath);
-  audioPlay.play();
-}
-
-$(".btn").on("click", function (e) {
+$(".btn").on("click", (e) => {
   numbClick++;
   const colorClicked = e.target.id;
-  colorAudio(colorClicked);
-  checkAnswer(colorClicked);
-  $("#" + colorClicked).addClass("pressed");
+  $("#" + colorClicked).addClass("hover");
   setTimeout(() => {
-    $("#" + colorClicked).removeClass("pressed");
-  }, 100);
+    $("#" + colorClicked).removeClass("hover");
+  }, 150);
+  gameAudio(colorClicked);
+  gameCheck(colorClicked);
 });
 
-function checkAnswer(colorClicked) {
+const gameCheck = (colorClicked) => {
   userPattern.push(colorClicked);
   if (colorClicked === correctPattern[numbClick]) {
     if (userPattern.length == correctPattern.length) {
       setTimeout(() => {
         userPattern = [];
-        numbClick = -1;
         gameSequence();
+        numbClick = -1;
       }, 1000);
     }
   } else {
-    $("h1").text("Game over! Press any key to start");
+    $("h1").text("Game over! Press Any key to start");
     $("body").addClass("game-over");
+    const wrongAudioPath = "sounds/wrong.mp3";
+    const wrongAudio = new Audio(wrongAudioPath);
+    wrongAudio.play();
     setTimeout(() => {
       $("body").removeClass("game-over");
-      let wrongAudioPath = "sounds/wrong.mp3";
-      let wrongAudio = new Audio(wrongAudioPath);
-      wrongAudio.play();
-    }, 150);
-    correctPattern = [];
-    userPattern = [];
-    numbClick = -1;
-    level = 0;
+    }, 200);
     if (level > highScore) {
       highScore = level;
       $("h4").text(`high score is ${highScore}`);
     }
+    correctPattern = [];
+    userPattern = [];
+    numbClick = -1;
+    level = 0;
   }
-}
+};
+
+const gameAudio = (randomColor) => {
+  const audioPath = `sounds/${randomColor}.wav`;
+  const colorAudio = new Audio(audioPath);
+  colorAudio.play();
+};
